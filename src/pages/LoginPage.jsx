5bdfabfc4dacf04b63dfc755d4b216c4a95b6bac
Rahul kumar <rahulkumarsah269@gmail.com>
2024-07-15 20:35:46 +0530

    created navbar

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -20,7 +20,7 @@ const LoginPage = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
-        }, 
+        },
         body: JSON.stringify({ email, password })
       })
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
-import {useNavigate} from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
 import "../styles/Register.scss";
+
 const RegisterPage = () => {
   const [formData, setFormData] = useState({
     firstName: "",
@@ -19,40 +20,38 @@ const RegisterPage = () => {
       [name]: name === "profileImage" ? files[0] : value,
     });
   };
-  console.log(formData);
-  
-  const [passwordMatch , setPasswordMatch] = useState(true)
+
+  const [passwordMatch, setPasswordMatch] = useState(true)
 
   useEffect(() => {
     setPasswordMatch(formData.password === formData.confirmPassword || formData.confirmPassword === "")
   })
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-    try{
+    try {
       const register_form = new FormData()
 
-      for(var key in formData){
+      for (var key in formData) {
         register_form.append(key, formData[key])
       }
 
-      //fetch the api
       const response = await fetch("http://localhost:3001/auth/register", {
         method: "POST",
         body: register_form
       })
 
-      if(response.ok){
+      if (response.ok) {
         navigate("/login")
       }
-    } catch(err) {
-      console.log("Registration failed!",err.message);
+    } catch (err) {
+      console.log("Registration failed", err.message)
     }
   }
-  //creating a registration form
+
   return (
     <div className="register">
       <div className="register_content">
@@ -82,47 +81,48 @@ const RegisterPage = () => {
           <input
             placeholder="Password"
             name="password"
-            type="password"
             value={formData.password}
             onChange={handleChange}
+            type="password"
             required
           />
           <input
             placeholder="Confirm Password"
             name="confirmPassword"
-            type="password"
             value={formData.confirmPassword}
             onChange={handleChange}
+            type="password"
             required
           />
 
           {!passwordMatch && (
-            <p style={{ color: "red" }}>Passwords are not match</p>
+            <p style={{ color: "red" }}>Passwords are not matched!</p>
           )}
+
           <input
             id="image"
             type="file"
             name="profileImage"
-            acccept="image/*"
+            accept="image/*"
             style={{ display: "none" }}
             onChange={handleChange}
             required
           />
           <label htmlFor="image">
             <img src="/assets/addImage.png" alt="add profile photo" />
-            <p>upload Your photo</p>
+            <p>Upload Your Photo</p>
           </label>
 
           {formData.profileImage && (
-            <img src={URL.createObjectURL(formData.profileImage)} 
-                alt="profile photo"
-                style={{maxWidth: "80px"}}
+            <img
+              src={URL.createObjectURL(formData.profileImage)}
+              alt="profile photo"
+              style={{ maxWidth: "80px" }}
             />
           )}
-
           <button type="submit" disabled={!passwordMatch}>REGISTER</button>
         </form>
-        <a href="/login">Already have an account? Log In here</a>
+        <a href="/login">Already have an account? Log In Here</a>
       </div>
     </div>
   );
